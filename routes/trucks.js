266fc74534b66fb318f2aff69c86f68dc14a58ee
Betty Reaney <bettyreaney@Bettys-MacBook-Pro.local>
2021-04-22 19:02:20 -0700

    add second view with menu

--- routes/trucks.js
+++ routes/trucks.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const truckCtrl = require('../controllers/trucks');
 
-router.get('/new', truckCtrl.new);
+router.get('/', truckCtrl.index);
+router.post('/', truckCtrl.new);
 
 module.exports = router;
